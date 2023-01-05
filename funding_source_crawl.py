2d87afda37d6135ef7ee0c94c290f7259fb29613
畑山翔斗 <>
2023-01-05 22:12:19 +0900

    aws環境でseleniumがエラーになったためchromedriver_binaryをimport

--- funding_source_crawl.py
+++ funding_source_crawl.py
@@ -7,6 +7,7 @@ import const
 import requests
 import logging
 from bs4 import BeautifulSoup
+import chromedriver_binary
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
