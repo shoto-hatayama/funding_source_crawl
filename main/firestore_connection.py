bf84cbf7d6d7aa425bdf20b8f09799b5e7161dc5
畑山翔斗 <>
2023-07-16 17:27:19 +0900

    ファイルの指定場所が間違っていたため正しいパスに修正

--- main/firestore_connection.py
+++ main/firestore_connection.py
@@ -6,13 +6,12 @@ from firebase_admin import credentials
 class FirestoreConnection:
     """firestoreの接続"""
 
-    __firebase_json_name = '../fundingsourceview-firebase-adminsdk-tq58w-82d2be4281.json'
+    __firebase_json_name = './fundingsourceview-firebase-adminsdk-tq58w-82d2be4281.json'
 
     def __init__(self):
         if not firebase_admin._apps:
             cred = credentials.Certificate(self.__firebase_json_name)
             firebase_admin.initialize_app(cred)
-
         self.__client = firestore.client().from_service_account_json(self.__firebase_json_name)
 
     def get_client(self):
