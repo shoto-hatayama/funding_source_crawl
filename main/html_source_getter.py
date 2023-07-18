eb1b273817b7bd4adf02f051918dfb0df4965134
畑山翔斗 <>
2023-07-19 00:50:45 +0900

    source_of_page_clickedメソッドをクラスに切り出し

---
+++ main/html_source_getter.py
@@ -0,0 +1,35 @@
+"""seleniumを使ってページ内の対象をクリックする"""
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+class HtmlSourceGetter:
+    """クリックした対象のhtnlを取得する"""
+    __driver_path = 'main/chromedriver'
+
+    def __init__(self,url):
+        options = Options()
+        options.add_argument('--disable-gpu')#GPUハードウェアアクセラレーションを無効
+        options.add_argument('--disable-extensions')#全ての拡張機能を無効
+        options.add_argument('--proxy-server="direct://"')#プロキシ経由せず直接接続
+        options.add_argument('--proxy-bypass-list=*')#プロキシサーバー経由しない
+        options.add_argument('--start-maximized')#初期のウィンドウサイズ最大化
+        options.add_argument('--headless')#ヘッドレスモードで起動
+
+        self.__driver = webdriver.Chrome(executable_path=self.__driver_path,chrome_options=options)
+        self.__driver.get(url)
+
+    def clicked_html(self,xpath: list):
+        """クリックする順番をxpathで指定してhtmlを取得"""
+
+        # フォームの「補助金・助成金・融資」を選択
+        for val in xpath:
+            element = self.__driver.find_element_by_xpath(val)
+            element.click()
+
+        source = self.__driver.page_source
+
+        # ブラウザとウィンドウを共に閉じる
+        self.__driver.close()
+        self.__driver.quit()
+
+        return source
---
+++ tests/test_html_source_getter.py
@@ -0,0 +1,13 @@
+from main.html_source_getter import HtmlSourceGetter
+
+def test_htmlsourcegetter_is_value():
+    """htmlが取得できたかチェック"""
+    click_target = [
+        '//*[@id="categorySelect"]/div/label[1]',#「補助金・助成金・融資」
+        '//*[@id="searchForm"]/div[9]/button[1]' #「検索実行」
+    ]
+    url = "https://j-net21.smrj.go.jp/snavi/articles"
+
+    html_source_getter = HtmlSourceGetter(url)
+
+    assert html_source_getter.clicked_html(click_target)
