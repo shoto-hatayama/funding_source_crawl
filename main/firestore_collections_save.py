f71cd1343c577733a8a08d0202a394931a397bd4
畑山翔斗 <>
2023-07-23 20:00:43 +0900

    エラー内容を詳細に表示するためtracebackモジュールを使う

--- main/firestore_collections_save.py
+++ main/firestore_collections_save.py
@@ -1,5 +1,6 @@
 """firestoreのコレクションの保存を管理"""
 import logging
+import traceback
 from firestore_connection import FirestoreConnection
 class FirestoreCollectionsSave:
     """firestoreのコレクションを保存"""
@@ -12,10 +13,5 @@ class FirestoreCollectionsSave:
         try:
             collection = self.__client.collection(collection_name)
             collection.add(crawl_data)
-            print(crawl_data + 'の保存が完了しました。')
-            logging.info("%s delete completed",collection_name)
-        except Exception as error_msg:
-            logging.error('★★★★★ここからエラー文★★★★★')
-            logging.error(error_msg)
-            logging.error('★★★★★ここまで★★★★★')
-
+        except Exception:
+            logging.error(traceback.format_exc())
